f73ac9c1d56f12147e1da8b6a7e27af70555ddf3
SiDevesh <sid.swapnildevesh@gmail.com>
2017-07-27 15:04:36 +0530

    Fix github routing

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import { Route, IndexRoute } from 'react-router';
+import { Route, IndexRedirect, IndexRoute } from 'react-router';
 import App from './containers/App/App';
 import Panorama from './containers/Panorama/Panorama';
 import PanoGallery from './containers/PanoGallery/PanoGallery';
 
 export default (
-  <Route path="/" component={App}>
-    <IndexRoute component={PanoGallery} />
-    <Route path="/:id" component={Panorama} />
+  <Route path="/">
+    <IndexRedirect to="PanoGallery"/>
+    <Route path="/PanoGallery" component={App}>
+      <IndexRoute component={PanoGallery} />
+      <Route path="/:id" component={Panorama}/>
+    </Route>
   </Route>
 );
